5e3adb8b40603ba7ad9c8ab6413d824d2071efdd
yuxino <nbsawz@gmail.com>
2020-03-29 00:17:07 +0800

    feat: done

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 #!/usr/bin/env node
 const { name, version } = require("./package.json");
 const program = require("commander");
+const lemuro = require("lemuro");
+const { Select } = require("enquirer");
+const { spawn } = require("child_process");
 
 program.version(`${name}/${version}`).parse(process.argv);
 
-console.log("wow !! waht a new cli app !");
+async function main() {
+  const pwd = process.env.PWD;
+  const json = `${pwd}/package.json`;
+
+  if (lemuro.isExists(json)) {
+    const buffer = await lemuro.readFile(json);
+    let choice;
+    try {
+      const data = JSON.parse(buffer);
+      if (data.scripts === undefined) {
+        console.log("😖 not any scripts can be execute");
+      } else {
+        const keys = Object.keys(data.scripts);
+        const prompt = new Select({
+          name: "script",
+          message: "Pick one script to execute",
+          choices: keys
+        });
+
+        choice = await prompt.run();
+
+        spawn("yarn", [choice], { stdio: "inherit" });
+      }
+    } catch (e) {
+      if (choice === undefined) {
+        console.log("🐦 okkk. bye");
+      } else {
+        console.log("😖 package.json has something wrong to be parse");
+      }
+    }
+  } else {
+    console.log("😖 package.json not found");
+  }
+}
+
+main();
